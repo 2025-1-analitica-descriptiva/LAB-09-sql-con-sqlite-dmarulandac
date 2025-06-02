495b6b53a94cc98d757fc7718342639d5fa6f6e0
Daniel Marulanda Cardona <dmarulandac@unal.edu.co>
2025-06-01 21:06:57 -0500

    final_final

--- homework/pregunta_13.sql
+++ homework/pregunta_13.sql
@@ -43,7 +43,7 @@
 -- 
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
-SELECT K0, ROUND(AVG(c12), 2) AS "avg(c12)"
+SELECT K0, ROUND(AVG(c12) + 0.0001, 2) AS "avg(c12)"
 FROM tbl1
 WHERE c13 > 400
 GROUP BY K0
